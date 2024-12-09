86f219d52f275e1b0cac2c0b626c46212cfaf08d
Danh Vo <danhvo.uit@gmail.com>
2024-12-09 23:13:12 +0700

    Refactor Users template

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):
-    return render(request, 'register.html')
+    return render(request, 'users/register.html')
